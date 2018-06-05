591d776acc18d40875c12438b40c2198890b7681
Tonsai jompun <tonsai.j@nexts-corp.com>
2018-06-05 13:48:47 +0700

    update @nylon

--- aqa-styles.js
+++ aqa-styles.js
@@ -1,5 +1,5 @@
 import '@polymer/polymer/polymer-element.js';
-import '@aqa/aqa-font/aqa-trirong-font.js';
+import '@nylon/aqa-font/aqa-trirong-font.js';
 import './flexbox-grid.js';
 const $_documentContainer = document.createElement('template');
 $_documentContainer.setAttribute('style', 'display: none;');
